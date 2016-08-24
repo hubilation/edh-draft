086e713d6287eb9714d50b7e36948835f2b11235
zchub261 <zack.huber@gmail.com>
2016-08-23 23:09:26 -0700

    created scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name edhDraftApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the edhDraftApp
+ */
+angular.module('edhDraftApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name edhDraftApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the edhDraftApp
+ */
+angular.module('edhDraftApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
